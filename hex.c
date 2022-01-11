454601974440a15488ba4eb973fd214dfe8743e2
YOPI <mrx.ga10@gmail.com>
2022-01-11 01:24:44 +0100

    now u can convert in both ways

--- hex.c
+++ hex.c
@@ -1,41 +1,57 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>
+#include <math.h>
 
 void	ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
-
-int	ft_atoi(char *p)
+void	ft_putstr(char *str)
 {
-	int i = 0;
-	int res = 0;
-	int sign = 1;
+	int	i;
 
-	while (p[i] == ' ' || p[i] == '\t' || p[i] == '\n' || p[i] == '\r' || p[i] == '\v' || p[i] == '\f')
+	if (!str)
+		return ;
+	i = 0;
+	while (str[i])
 	{
+		write(1, &str[i], 1);
 		i++;
 	}
-	if (p[i] == '-' || p[i] == '+')
-	{
-		if (p[i] == '-')
-			sign = sign * -1;
-		i++;
-	}
-	while (p[i] >= '0' && p[i] <= '9')
-	{
-		res = res *10 + (p[i] - '0');
-		i++; 
-	}
-	return (res * sign);
+}
 
+void	ft_dec(char *str)
+{
+	int	sum;
+	int	j;
+	int	len;
+
+	sum = 0;
+	j = 0;
+	len = (strlen(str) - 1);
+	while (len >= 0)
+	{
+		if (str[len] >= '0' && str[len] <= '9')
+		{
+			sum = sum + (str[len] - 48) * pow(16, j);
+			j++;
+		}
+		else if (str[len] >= 'A' && str[len] <= 'F')
+		{
+			sum = sum + (str[len] - 55) * pow(16, j);
+			j++;
+		}
+		len--;
+	}
+	printf("Decimal Value is %d", sum);
 }
 
 void	ft_hex(int x)
 {
 	if (x == 0)
 		ft_putchar('0');
-	char hex[16] = "0123456789abcdef";
+	char hex[16] = "0123456789ABCDEF";
 	int save[10];
 	int here = 0;
 
@@ -45,6 +61,7 @@ void	ft_hex(int x)
 		here++;
 		x = x / 16;
 	}
+	ft_putstr("Hex Value is ");
 	while (here > 0)
 	{
 		ft_putchar(hex[save[here-1]]);
@@ -52,14 +69,29 @@ void	ft_hex(int x)
 	}
 }
 
-int	main(int ac, char **av)
+int	main(void)
 {
-	if (ac == 2)
+	char	hex[1000];
+	int		choice;
+	int 	dec;
+	
+    printf("For Decimal to HEX Type: 1\n");
+	printf("For HEX to Decimal Type: 2\n");
+	scanf("%d", &choice);
+	if (choice == 1)
 	{
-		int i;
-		
-		i = ft_atoi(av[1]);
-		ft_hex(i);
+		printf("Please! Enter the Decimal value :");
+		scanf("%d", &dec);
+		ft_hex(dec);
 	}
+	else if(choice == 2)
+	{
+		printf("Please! Enter the HEX value :");
+		scanf("%s", hex);
+		ft_dec(hex);
+	}
+	else
+		printf("404 try again");
+
 	ft_putchar('\n');
 }
